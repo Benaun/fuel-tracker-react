c1e92b2e4ad52c003a2763a1160de8041a2ef9f5
Andrey <Andrey.kuhtin126@gmail.com>
2024-02-18 21:18:14 +0300

    refactoring

--- components/slider.jsx
+++ components/slider.jsx
@@ -1,25 +1,13 @@
-import { Carousel,Typography } from "@material-tailwind/react";
+import { Carousel, Typography } from "@material-tailwind/react";
 
 export function Slider() {
     return (
         <Carousel className="rounded-xl h-5/6">
-            <div>
-                <img
-                    src="/cars/Coolray.png"
-                    alt="image 1"
-                    className="h-full w-full object-cover"
-                />
-                <div>
-                    <Typography
-                        variant="h1"
-                        color="white"
-                        className="mb-4 text-3xl md:text-4xl lg:text-5xl"
-                    >
-                        The Beauty of Nature
-                    </Typography>
-                </div>
-            </div>
-
+            <img
+                src="/cars/Coolray.png"
+                alt="image 1"
+                className="h-full w-full object-cover"
+            />
             <img
                 src="/cars/Coolray.png"
                 alt="image 2"
